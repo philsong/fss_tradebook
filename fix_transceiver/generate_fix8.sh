33b53375c3e1f835381f3cf89424c97583fa056d
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2015-11-30 02:36:41 -0500

    working on session configuration for fix8

--- fix_transceiver/generate_fix8.sh
+++ fix_transceiver/generate_fix8.sh
@@ -1,2 +1,2 @@
-f8c -p fixwrap -n FIXWRAP -c client -o client FIX44.xml
-f8c -p fixwrap -n FIXWRAP -c server -o server FIX44.xml
+f8c -p fixclient -n CLIENTWRAP -c client -o client FIX44.xml
+f8c -p fixserver -n SERVERWRAP -c server -o server FIX44.xml

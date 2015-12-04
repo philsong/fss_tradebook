33bc4c5c8838e01378f54216c37a4449a496b9a3
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2015-12-04 18:22:07 -0500

    Added fix_transceiver files

--- fix_transceiver/client/fix_client.cpp
+++ fix_transceiver/client/fix_client.cpp
@@ -1,5 +1,10 @@
+#include "fixclient_session.hpp"
+#include <sessionwrapper.hpp>
+
 // Client Session Main
 int main(int argc, char const *argv[]) {
+
+	using namespace FIX8;
 
 	ClientSession<fixclient_session_client>::ClientSession_ptr mc;
 	mc = new ClientSession<fixclient_session_client>(CLIENTWRAP::ctx(), 
@@ -8,3 +13,7 @@ int main(int argc, char const *argv[]) {
 
 	return 0 ;
 }
+
+// Closest thing to compiling:
+// g++ fix_client.cpp -I/usr/include/fix8 -lfix8 -std=c++11
+

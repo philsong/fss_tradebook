4d0631e775f8899d0726808800b7d59b2cdba751
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2015-12-05 12:27:37 -0500

    Expanding schema for various order types

--- schema.sql
+++ schema.sql
@@ -17,14 +17,39 @@ CREATE TABLE Trade(
 	FOREIGN KEY(tid) REFERENCES Traders(id)
 );
 
-CREATE TABLE market_order(
+-- option 1: trade type in trade table, secondary table for limit_order keeps
+-- track of limit for a certain order_id
 
+CREATE TABLE market_order(
+	order_id serial PRIMARY KEY,
+	symbol text,
+	expiry_month int,
+	expiry_year int,
+	trader_id int FOREIGN KEY REFERENCES Traders(id),
+	transaction_time timestamp,
+	action -- buy / sell
 );
 
 CREATE TABLE limit_order(
-
+	order_id serial PRIMARY KEY,
+	symbol text,
+	expiry_month int,
+	expiry_year int,
+	trader_id int FOREIGN KEY REFERENCES Traders(id),
+	transaction_time timestamp,
+	action -- buy / sell
 );
 
 CREATE TABLE pegged_order(
+	order_id serial PRIMARY KEY,
+	symbol text,
+	expiry_month int,
+	expiry_year int,
+	trader_id int FOREIGN KEY REFERENCES Traders(id),
+	transaction_time timestamp,
+	action -- buy / sell
+);
+
+CREATE TABLE Fills(
 
 );
